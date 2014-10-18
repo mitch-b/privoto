86e54f9aa2834a180c7e69e288494d715ba1d964
Mitchell Barry <mitch.barry@gmail.com>
2014-10-17 22:35:07 -0500

    made directory storage clearer

--- src/main/java/com/mitchbarry/privoto/core/controller/api/MessageController.java
+++ src/main/java/com/mitchbarry/privoto/core/controller/api/MessageController.java
@@ -24,7 +24,7 @@ public class MessageController {
 
     private Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
 
-    @Value("${upload.dir}")
+    @Value("${storage.dir}")
     private String uploadDirectory;
 
     @RequestMapping(value = "/api/message/image", method = RequestMethod.POST)
